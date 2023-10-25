aa7c4f86de0407cc8e08d553ca0f31769a585b1e
Denis Kaminsky <kkaminsky.denis@gmail.com>
2023-10-25 22:38:30 +0200

    Add project files.

---
+++ EncryptionAndHashAlgorithms/MainForm.cs
@@ -0,0 +1,10 @@
+namespace EncryptionAndHashAlgorithms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
